d9026a39236a1b4887112f83a19caa4ac11ea7ef
Alexandria <alxpettit@gmail.com>
2021-12-31 13:45:59 -0800

    whoops, forgot to commit latest changes

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ const bot = mineflayer.createBot({
     // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
 })
 
+function player_filter(entity) {
+    return entity.type === 'player'
+}
+
+function look_at_nearest_player() {
+
+    bot.setControlState('forward', true)
+    //if (bot.blockAt(bot.entity.position.offset(0, 0, 1))) {
+    bot.setControlState('jump', true)
+    //}
+    const player_entity = bot.nearestEntity(player_filter)
+    if (player_entity) {
+        const player_entity_pos = player_entity.position;
+        const player_eye_pos = player_entity_pos.offset(0, player_entity.height, 0)
+        bot.lookAt(player_eye_pos)
+        if (bot.entity.position.distanceTo(player_entity_pos) > 2) {
+        } else {
+            bot.attack(player_entity)
+        }
+    }
+    //bot.clearControlStates()
+}
+
+bot.on('physicTick', look_at_nearest_player)
 //
 // bot.on('chat', (username, message) => {
 //     if (username === bot.username) return
